33d44c9de3cdce6c3c2ee88646e7a7a54df1aab4
Matthew White <matt@substructu.re>
2014-03-02 12:24:45 -0500

    Adding component toggle for aside and overlay

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,36 @@
-//= require_tree .
+(function () {
+
+  'use strict'
+
+  var n;
+
+  $(document).ready(function () {
+
+    n.Toggle.listeners();
+
+  }),
+
+  n = {
+
+    Toggle: {
+
+      listeners: function() {
+        $('.toggle').on('click', n.Toggle.toggle_component);
+      },
+
+      toggle_component: function(e) {
+        var target = $(this).data('target');
+
+        if ($('.page.active--aside').length) {
+          $('.page').removeClass('active--aside');
+          $('.page').removeClass('active--overlay');
+        }
+        else {
+          $('.page').addClass('active--aside');
+          $('.page').addClass('active--overlay');
+        }
+      }
+    }
+  }
+
+}());
