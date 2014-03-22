3a8a7429996a9634f5d9891d7145b43acabd7c9b
Matthew White <matt@substructu.re>
2014-03-22 15:58:38 -0400

    Abstracting site title from title helper

--- helpers/site_helpers.rb
+++ helpers/site_helpers.rb
@@ -1,15 +1,17 @@
 module SiteHelpers
-
+  
   def page_title
     title = ""
 
     if current_page.url == "/"
-      title = "What Red Read"
+      title = data.site["title"]
     else
       if data.page.title
-        title = data.page.title + " | What Red Read"
+        title = data.page.title + " | " + data.site["title"]
+      elsif yield_content(:title)
+        title = yield_content(:title) + " | " + data.site["title"]
       else
-        title = "What Red Read"
+        title = data.site["title"]
       end
     end
     title
