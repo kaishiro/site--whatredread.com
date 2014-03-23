98633af4152889c559381525ea1c3335e33969b8
Matthew White <matt@substructu.re>
2014-03-23 11:24:25 -0400

    Adding hash scroll animation

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -22,6 +22,17 @@
     n.Toggle.listeners();
     n.Hover.listeners();
 
+    $('.scroll[href^="#"]').bind('click.smoothscroll', function (e) {
+      e.preventDefault();
+      var target = this.hash;
+      var $target = $(target);
+      $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+      }, 1000, 'swing', function () {
+        window.location.hash = target;
+      });
+
+    });
 
   }),
 
