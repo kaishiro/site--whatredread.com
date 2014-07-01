c9e564b8dfafb47de71bd815e0cfd9c9d5b17633
Matthew White <matt@substructu.re>
2014-07-01 17:08:55 -0400

    Re-architecting paths

--- source/javascripts/search.js
+++ source/javascripts/search.js
@@ -1,36 +1,66 @@
 $(function() {
-  return $('.search-submit').on('click', function(e) {
-    var search_term;
-    e.preventDefault();
-    search_term = $('.search-term').val().toLowerCase();
-    return $.getJSON('/search/entries.json', function(data) {
-      var i, result, results, value, _i, _j, _len, _len1, _results;
-      results = [];
-      for (_i = 0, _len = data.length; _i < _len; _i++) {
-        i = data[_i];
-        value = 0;
-        if (i.title.toLowerCase().split(search_term).length - 1 !== 0) {
-          value = 10;
-        }
-        //if (i.content.toLowerCase().split(search_term).length - 1 !== 0) {
-        //  value += (i.content.toLowerCase().split(search_term).length - 1) * 5;
-        //}
-        if (value !== 0) {
-          i.value = value;
-          results.push(i);
-        }
+
+  'use strict'
+
+  var n;
+
+  $(document).ready(function () {
+    n.Search.listeners();
+  }),
+
+  n = {
+
+    Search: {
+
+      listeners: function() {
+        $('.search .search__submit').on('click', n.Search.return_results);
+      },
+
+      return_results: function(e) {
+
+        var _searchInput = $('.search .search__input');
+        var _searchTerms = _searchInput.val().toLowerCase();
+        var _searchResults = $('.search .seach__results');
+        var _searchEntries = '/search/entries.json';
+
+        e.preventDefault();
+
+        return $.getJSON(_searchEntries, function(data) {
+
+          var i, result, results, value, _i, _j, _l, _len1, _results;
+
+          results = [];
+
+          for (_i = 0, _l = data.length; _i < _l; _i++) {
+            i = data[_i];
+            value = 0;
+            if (i.title.toLowerCase().split(_searchTerms).length - 1 !== 0) {
+              value = 10;
+            }
+            //if (i.content.toLowerCase().split(search_term).length - 1 !== 0) {
+            //  value += (i.content.toLowerCase().split(search_term).length - 1) * 5;
+            //}
+            if (value !== 0) {
+              i.value = value;
+              results.push(i);
+            }
+          }
+
+          _searchResults.html('');
+          if (results.length > 0) {
+            _results = [];
+            for (_j = 0, _len1 = results.length; _j < _len1; _j++) {
+              result = results[_j];
+              _results.push($('.search .search__results').append('<p><a href="/' + result.url + '">' + result.title + '</a></p>'));
+            }
+            return _results;
+          } else {
+            $('.search .search__results').append('<p>No results found. Sorry.</p>');
+          }
+        });
       }
-      $('.search-results').html('');
-      if (results.length > 0) {
-        _results = [];
-        for (_j = 0, _len1 = results.length; _j < _len1; _j++) {
-          result = results[_j];
-          _results.push($('.search-results').append('<p><a class="copy-bg" href="/' + result.url + '">' + result.title + '</a></p>'));
-        }
-        return _results;
-      } else {
-        $('.search-results').append('<p>No results found. Sorry.</p>');
-      }
-    });
-  });
-});
+    }
+  }
+
+}());
+
